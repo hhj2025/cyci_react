3ea327bcb55e8e67e26b8a1c5139dcd47f7d95f1
HHJ <hhj2025>
2025-12-17 13:38:07 +0900

    학생 정보 리스트 추가

--- src/page/StudentInfo.jsx
+++ src/page/StudentInfo.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import data from '../data/Student.json';
+import stu from '../data/Student.json';
 
 function StudentInfo() {
 
@@ -8,6 +8,28 @@ function StudentInfo() {
   return (
     <>
       <h1>학생 정보 리스트</h1>
+      <table style={{border : '1px solid black'}}>
+        <thead>
+            <tr>
+                <th>이름</th>
+                <th>나이</th>
+                <th>이메일</th>
+                <th>지역</th>
+            </tr>
+        </thead>
+        <tbody>   
+            {stu.student.map(stu => {
+                return(
+                    <tr key={stu.id}>
+                        <td>{stu.name}</td>
+                        <td>{stu.age}</td>
+                        <td>{stu.email}</td>
+                        <td>{stu.region}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+      </table>
     </>
   )
 }
