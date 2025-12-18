c508d81a3a0faa0a112a2bb86d1f15d79253c457
HHJ <hhj2025>
2025-12-18 10:17:57 +0900

    자식 건네주기 2

--- src/page/자식건네주기.jsx
+++ src/page/자식건네주기.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 function 자식건네주기() {
 
   let msg = 'Hello World';
   let obj = {title : '책 제목', content : '책 내용'};
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <h1>자식과 부모의 데이터 전달</h1>
-      <자식 dataMsg={msg} dataBook={obj}/>
+      <자식 dataMsg={msg} dataBook={obj} dataCount={count}/>
+      <자식2/>
+      <input type="button" value="숫자 증가" onClick={ e => {
+        e.preventDefault();
+        setCount(count+1);
+      }}/>
     </>
   )
 }
 
 export default 자식건네주기
+
+function 자식2(props) {
+  return(
+    <div style={{width : '100vw', height : '200px', border : '2px solid blue'}}>
+      <h3>자식 Component 영역</h3>
+    </div>
+  )
+}
 
 function 자식(props) {
   return(
@@ -22,6 +38,8 @@ function 자식(props) {
       책 제목 : {props.dataBook.title}
       <br/>
       책 내용 : {props.dataBook.content}
+      <br/>
+      count : {props.dataCount}
     </div>
   )
 }
