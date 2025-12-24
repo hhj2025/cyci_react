3f8641a75ef3249b4cd13ee9e8532602d214e27c
HHJ <hhj2025>
2025-12-24 10:04:13 +0900

    게시판등록

--- src/page/axios/Axios보내기.jsx
+++ src/page/axios/Axios보내기.jsx
@@ -1,6 +1,9 @@
-import axios from "axios"
+import axios from "axios";
+import { useState } from "react";
 
 function Axios보내기() {
+
+  const [list, setList] = useState([]);
 
   function test1() {
     axios.get('http://localhost:8080/test1')
@@ -44,6 +47,25 @@ function Axios보내기() {
     });
   }
 
+    function boardList() {
+    axios.get('http://localhost:8080/api/board/list')
+    .then(res => {
+      console.log(res);
+      if(res.status !== 200){
+        console.log("error");
+      }
+      else{
+        const list = res.data;
+        console.log(list);
+        setList(list);
+      }
+    });
+  }
+
+  function eventData(){
+    boardList();
+  }
+
   return (
     <>
       <h1>Axios 사용해보기</h1>
@@ -58,9 +80,45 @@ function Axios보내기() {
       <input type="button" value="Axios res 보내기" onClick={res1} />
 
 
+    <h1>게시판 작성하기</h1>
+    <RegisterBoard event={eventData}/>
+
+    <h1>게시판 불러오기</h1>
+    <input type="button" value="게시판 불러오기" onClick={boardList} />
+    {list.map(item => (
+      <div key={item.boardIdx}>
+        <span>{item.title}</span>
+        <span>{item.userId}</span>
+        <span>{item.createAt}</span>
+      </div>
+    ))}
 
     </>
   )
 }
 
 export default Axios보내기
+
+function RegisterBoard(){
+
+    function BoardRegister(props) {
+
+    const title = document.getElementById("title");
+    const userId = document.getElementById("userId");
+    const content = document.getElementById("content");
+
+    const obj = {title : title.value, userId : userId.value, content : content.value};
+    axios.post('http://localhost:8080/api/board/register', obj)
+    .then(res => {
+      console.log(res);
+       props.event && props.event();
+    })}
+  return(
+    <>
+      제목 : <input type="text" id="title"/><br/>
+      작성자 : <input type="text" id="userId"/><br/>
+      내용 : <textarea style={{width : '300px', height : '200px'}} id="content"></textarea>
+      <input type="button" value="게시판 등록" onClick={BoardRegister} />
+    </>
+  )
+}
